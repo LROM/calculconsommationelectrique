7b9a51a22a834346e0d3140db9561dede917a362
liliana <liliana_rosar@hotmail.com>
2022-03-15 23:23:07 -0400

    set header in all pages

--- sign_up.php
+++ sign_up.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- <link rel="stylesheet" href="mystyle.css" type="txt/css"> -->
         
-        <link rel="stylesheet" href="mystyle2.css">
+        <link rel="stylesheet" href="mystyle.css">
     
     </head>
     <body>
