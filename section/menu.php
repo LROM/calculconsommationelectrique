c2aab31a5d4d5a67b57eef89e4453335c7b36901
liliana <liliana_rosar@hotmail.com>
2022-03-15 23:42:14 -0400

    set style of menu

--- section/menu.php
+++ section/menu.php
@@ -1,8 +1,8 @@
-<div id ="menu">
-    <a href="index.php">Accueil</a>
-    <a href="login.php">Login</a>
-    <a href="sign_up.php">Sign up</a>
-    <a href="setbills.php">Entree de donnees</a>
-    <a href="setconsomation.php">Entree de facture</a>
-    <a href="evaluation.php">Evaluation/Comparaison</a>
+<div class = "menu">
+    <a class = "menu_option" href="index.php">Accueil</a>
+    <a class = "menu_option" href="login.php">Login</a>
+    <a class = "menu_option" href="sign_up.php">Sign up</a>
+    <a class = "menu_option" href="setbills.php">Entree de donnees</a>
+    <a class = "menu_option" href="setconsomation.php">Entree de facture</a>
+    <a class = "menu_option" href="evaluation.php">Evaluation/Comparaison</a>
 </div>  
