4cfc7360400e6eb96bc50303fb2760dc419be8a0
liliana <liliana_rosar@hotmail.com>
2022-03-30 00:21:43 -0400

    refactor of database

--- db/appconso.sql
+++ db/appconso.sql
@@ -7,8 +7,8 @@ USE appconso;
 
 create table if not exists utilisateur (
 	id bigint auto_increment primary key,
-	name varchar(128) charset utf8,
 	username varchar(128) charset utf8,
+	courriel varchar(128) charset utf8,
 	password varchar(128) charset utf8
 ) engine=InnoDB default charset latin1;
 
@@ -22,30 +22,37 @@ create table if not exists  maison(
 ) engine=InnoDB default charset latin1;
 
 
-create table if not exists apareil (
+create table if not exists appareil (
 	id bigint auto_increment primary key,
 	name varchar(128) charset utf8,
-	watts_heure int,
-	heures_printemps int,
-	heures_ete int,
-	heures_aoutomme int,
-	heures_hiver int
+	kilowatts_heure DECIMAL (6,2) UNSIGNED not null
 ) engine=InnoDB default charset latin1;
 
-create table maison_appareil(
+
+create table if not exists consommation(
+	id bigint auto_increment primary key,
     id_maison bigint not null,
     id_appareil bigint not null,
-    primary key(id_maison, id_appareil),
-    CONSTRAINT fk_maison_appareil_maison foreign key (id_maison) references maison (id) ON DELETE CASCADE ON UPDATE CASCADE, 
-    CONSTRAINT fk_maison_appareil_appareil foreign key (id_appareil) references apareil (id) ON DELETE CASCADE ON UPDATE CASCADE
+	heures_par_jour_printemps int,
+	heures_par_jour_ete int,
+	heures_par_jour_automme int,
+	heures_par_jour_hiver int,
+    CONSTRAINT fk_consommation_maison foreign key(id_maison) references maison(id) ON DELETE CASCADE ON UPDATE CASCADE, 
+    CONSTRAINT fk_consommation_appareil foreign key(id_appareil) references appareil(id) ON DELETE CASCADE ON UPDATE CASCADE
+) engine=InnoDB default charset latin1;
+
+create table if not exists tarif_kwh (
+	id bigint auto_increment primary key,
+	kilowatts_heure DECIMAL (6,2) UNSIGNED not null
 ) engine=InnoDB default charset latin1;
 
 
-
-INSERT INTO utilisateur VALUES(NULL, 'Lili_user','lili','pass');
+INSERT INTO utilisateur VALUES(NULL, 'lili','lili@test.com','pass');
 
 INSERT INTO maison VALUES(NULL, 'shawi 123','g8p 6j9',1);
 
-INSERT INTO apareil VALUES(NULL, 'congelador',4 , 1, 6, 1, 1);
+INSERT INTO appareil VALUES(NULL, 'congelador', 0.50 );
 
-INSERT INTO maison_appareil VALUES(1, 1);
+INSERT INTO consommation VALUES(NULL, 1, 1, 1, 6, 1, 1);
+
+INSERT INTO tarif_kwh VALUES(NULL, 2.22);
