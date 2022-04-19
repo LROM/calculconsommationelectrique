fde08eadee3983487fb02fc3ecd2661171305ff2
liliana <liliana_rosar@hotmail.com>
2022-04-19 00:25:13 -0400

    add gestion des appareils to navigation menu

--- section/menu-logged.php
+++ section/menu-logged.php
@@ -3,5 +3,6 @@
     <a class = "menu_option" href="entreedonnees.php">Entree de donnees</a>
     <a class = "menu_option" href="entreefacture.php">Entree de facture</a>
     <a class = "menu_option" href="evaluation.php">Evaluation/Comparaison</a>
+    <a class = "menu_option" href="gestionappareil.php">Gestion des appareils</a>
     <a class = "menu_option" href="index.php">logout</a>
 </div>  
