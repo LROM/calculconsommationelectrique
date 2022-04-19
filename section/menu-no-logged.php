f835a958c3581b8e5be3bf7cbfad86d4ad5f06f9
liliana <liliana_rosar@hotmail.com>
2022-04-19 00:25:57 -0400

    delete sing up option from navigation menu

--- section/menu-no-logged.php
+++ section/menu-no-logged.php
@@ -1,5 +1,4 @@
 <div class = "menu">
     <a class = "menu_option" href="index.php">Accueil</a>
     <a class = "menu_option" href="login.php">Login</a>
-    <a class = "menu_option" href="sign_up.php">Sign up</a>
 </div>  
