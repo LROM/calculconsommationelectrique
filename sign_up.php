aeb2684476f0d767aff4d4f2da42f78e728919ab
liliana <liliana_rosar@hotmail.com>
2022-04-20 00:27:39 -0400

    format sign_up page

--- sign_up.php
+++ sign_up.php
@@ -61,43 +61,43 @@ $utilisateurRepo = new UtilisateurRepository($config);
 
         <div class="panel">
 
-        <div class="panel-title">
+            <div class="panel-title">
 
-        </div>
-        <div class="panel-body">
+            </div>
+            <div class="panel-body">
 
-            <form class="form" action="/sign_up.php" method="post">
-                <h1>Sign up</h1>
-                <!--<p class "form_paragraph"> New user <a href="#" class="form_link">Sign up</a></p>-->
+                <form class="form" action="/sign_up.php" method="post">
+                    <h1>Sign up</h1>
+                    <!--<p class "form_paragraph"> New user <a href="#" class="form_link">Sign up</a></p>-->
 
-                <div class="form_container">
-                    <div class="form_group">
-                        <input type="text" id="name" name="username" class="form_input" placeholder=" ">
-                        <label for="name" class="form_label">User Name:</label>
-                        <span class="form_line"></span>
+                    <div class="form_container">
+                        <div class="form_group">
+                            <input type="text" id="name" name="username" class="form_input" placeholder=" ">
+                            <label for="name" class="form_label">User Name:</label>
+                            <span class="form_line"></span>
+                        </div>
+                        <div class="form_group">
+                            <input type="text" id="courriel" name="courriel" class="form_input" placeholder=" ">
+                            <label for="name" class="form_label">Email:</label>
+                            <span class="form_line"></span>
+                        </div>
+                        <div class="form_group">
+                            <input type="password" id="password" name="password" class="form_input" placeholder=" ">
+                            <label for="name" class="form_label">Password:</label>
+                            <span class="form_line"></span>
+                        </div>
+                        <div class="form_group">
+                            <input type="password" id="password2" name="password2" class="form_input" placeholder=" ">
+                            <label for="name" class="form_label">Repeat password:</label>
+                            <span class="form_line"></span>
+                        </div>
+
+                        <input type="submit" class="form_submit" name="button_sign_up" value="Submit">
+
                     </div>
-                    <div class="form_group">
-                        <input type="text" id="courriel" name="courriel" class="form_input" placeholder=" ">
-                        <label for="name" class="form_label">Email:</label>
-                        <span class="form_line"></span>
-                    </div>
-                    <div class="form_group">
-                        <input type="password" id="password" name="password" class="form_input" placeholder=" ">
-                        <label for="name" class="form_label">Password:</label>
-                        <span class="form_line"></span>
-                    </div>
-                    <div class="form_group">
-                        <input type="password" id="password2" name="password2" class="form_input" placeholder=" ">
-                        <label for="name" class="form_label">Repeat password:</label>
-                        <span class="form_line"></span>
-                    </div>
 
-                    <input type="submit" class="form_submit" name="button_sign_up" value="Submit">
-
-                </div>
-
-            </form>
-        </div>
+                </form>
+            </div>
             <?php
             require_once 'section/retroaction.php';
             ?>
