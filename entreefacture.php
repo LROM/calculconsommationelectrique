538e0c50f21310bb41e0ced23d1af4f550f69bab
liliana <liliana_rosar@hotmail.com>
2022-04-22 00:38:57 -0400

    set format to entrrefacture.php

--- entreefacture.php
+++ entreefacture.php
@@ -4,42 +4,45 @@ session_start();
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title> Mon app</title>
-        
-        <link rel="stylesheet" href="mystyle.css">
 
-        
-        <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Mon app</title>
+
+    <link rel="stylesheet" href="mystyle.css">
+
+
+    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>-->
-    </head>
-    <body>
+</head>
 
-            <?php
-            require_once 'section/header.php';
-            ?>
-            
+<body>
+
+    <?php
+    require_once 'section/header.php';
+    ?>
 
 
-            <div class="container">
-            <?php
-            require_once 'section/menu-logged.php';
-            ?>
-            <div class="panel">
+
+    <div class="container">
+        <?php
+        require_once 'section/menu-logged.php';
+        ?>
+        <div class="panel">
             <div class="panel-title">
-                
+
                 <h1>Entree de facture</h1>
             </div>
             <div class="panel-body">
             </div>
 
-            </div>
-            </div>
-            <?php
-            require_once 'section/footer.php';
-            ?>
+        </div>
+    </div>
+    <?php
+    require_once 'section/footer.php';
+    ?>
 
-    </body>
+</body>
+
 </html>
