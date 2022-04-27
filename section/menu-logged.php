35efee3dbd1cf1be29569c77c87cc5c64b318b08
liliana <liliana_rosar@hotmail.com>
2022-04-27 08:31:11 -0400

    change menu-logged.php

--- section/menu-logged.php
+++ section/menu-logged.php
@@ -11,9 +11,9 @@
     <p class="utilisateur"> Utilisateur: <?= $_SESSION["username"] ?> </p>
     <ul>
       <li><a class = "menu_option" href="account.php">Account</a></li>
-      <li><a class = "menu_option" href="configurationmaison.php">Configuration de maison</a></li>
-      <li><a class = "menu_option" href="entreefacture.php">Entree de facture</a></li>
+<!--      <li><a class = "menu_option" href="configurationmaison.php">Configuration de maison</a></li> -->
       <li><a class = "menu_option" href="evaluation.php">Evaluation/Comparaison</a></li>
+      <li><a class = "menu_option" href="entreefacture.php">Entree de facture</a></li>
       <li><a class = "menu_option" href="gestionappareil.php">Gestion des appareils</a></li>
       <li><a class = "menu_option" href="index.php">logout</a></li>
     </ul>
